f1eab90eed059f03f5ff30d0379a5c235d82b101
Sumit09988 <sumit999777555@gmail.com>
2026-07-18 10:23:13 +0530

    Add files via upload

---
+++ deepseek_python_20260718_c9174c.py
@@ -0,0 +1,17 @@
+# ============================
+# BOT TOKENS - UPDATED
+# ============================
+BOT_TOKEN = "8696235077:AAH3cWECRbpRoLc_FjYzMJt_Iwr8-21RDyE"
+OWNER_BOT_TOKEN = "8696235077:AAH3cWECRbpRoLc_FjYzMJt_Iwr8-21RDyE"  # same token
+OWNER_CHAT_ID = 8242927146
+
+# ============================
+# BOT NAME
+# ============================
+BOT_NAME = "<b>𝗦𝗨𝗠𝗜𝗧 𝗕𝗢𝗧</b>"
+
+# ============================
+# FORCE JOIN CHANNEL
+# ============================
+CHANNEL_USERNAME = "@bmw_slotss"
+CHANNEL_URL = "https://t.me/bmw_slotss"
